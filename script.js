95234af5df08f4ee03bfe716538e3d253f20f9de
bfly2001 <brendan.sflynn@gmail.com>
2020-09-21 15:52:04 -0700

    reset width to iframe. added js for sidebar.
    reset width to iframe. added js for sidebar.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+document.onclick = function(e) {
+    if(e.target.id == 'mySidebar') {
+        return false;
+    }
+    closeNav();
+}
+
 function openNav() {
     document.getElementById('mySidebar').style.width = "250px";
     document.getElementById('main').style.marginLeft = "250px";
