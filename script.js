22c262a3c959c00be175c9d4a26c74b753c226b0
bfly2001 <brendan.sflynn@gmail.com>
2020-09-27 12:33:24 -0700

    Update script.js
    -added class 'open' to sidebar in openNav().
    
    -removed class 'open' from sidebar in closeNav.

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const main = document.getElementById('main');
 function closeNav() {
     sidebar.style.width = "0";
     main.style.marginLeft = "0";
+    sidebar.classList.remove('open');
 }
 
 function openNav() {
     sidebar.style.width = "250px";
     main.style.marginLeft = "250px";
+    sidebar.classList.add('open');
 }
-
