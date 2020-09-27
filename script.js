07ada268ac5392e9d015f244e67fadd0832970c5
bfly2001 <brendan.sflynn@gmail.com>
2020-09-27 11:31:46 -0700

    Update script.js
    declared variables sidebar and main. used in open/close function below.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const sidebar = document.getElementById('mySidebar');
+const main = document.getElementById('main');
+
 function closeNav() {
-    document.getElementById('mySidebar').style.width = "0";
-    document.getElementById('main').style.marginLeft = "0";
+    sidebar.style.width = "0";
+    main.style.marginLeft = "0";
 }
 
 function openNav() {
-    document.getElementById('mySidebar').style.width = "250px";
-    document.getElementById('main').style.marginLeft = "250px";
+    sidebar.style.width = "250px";
+    main.style.marginLeft = "250px";
 }
+
