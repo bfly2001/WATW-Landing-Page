7421ea0f7c8e6a866406685788950ff732402b0e
bfly2001 <brendan.sflynn@gmail.com>
2020-09-28 10:08:43 -0700

    Update script.js
    added classList code to openNav() and closeNav() and added event listener to 'main'.

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ function closeNav() {
     sidebar.style.width = "0";
     main.style.marginLeft = "0";
     sidebar.classList.remove('open');
+    main.classList.remove('closed');
 }
 
 function openNav() {
     sidebar.style.width = "250px";
     main.style.marginLeft = "250px";
     sidebar.classList.add('open');
+    main.classList.add('closed');
 }
+
+main.addEventListener("click", () => {
+    if(sidebar.classList.contains('open')) {
+        sidebar.style.width = "0";
+        main.style.marginLeft = "0";
+    }
+});
